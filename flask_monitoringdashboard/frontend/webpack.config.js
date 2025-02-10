766e1973ff2334f1334fd4aafff44100bb873037
AlbertRossJoh <albertrossj@gmail.com>
2025-02-10 11:20:39 +0100

    removed uneeded change

--- flask_monitoringdashboard/frontend/webpack.config.js
+++ flask_monitoringdashboard/frontend/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '../static'),
         filename: 'js/app.js',
-        publicPath: path.resolve(__dirname, '../static/'),
+        publicPath: '/static/'
     },
     module: {
         rules: [
